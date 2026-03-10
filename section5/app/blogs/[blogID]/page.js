9ef38d2083895ff6e7522431d1f1166e795c9acc
Your Name <user@example.com>
2026-03-10 19:52:21 +0530

    s5 till part3

--- section5/app/blogs/[blogID]/page.js
+++ section5/app/blogs/[blogID]/page.js
@@ -1,16 +1,12 @@
 const Blog = async ({ params }) => {
   const { blogID } = await params;
 
-  // if (blogID % 2 === 0) {
-  //   throw new Error("BlogId can only be an odd number");
-  // }
-
-  const randomNumber = Math.random();
-  console.log(randomNumber);
+  // const randomNumber = Math.random();
+  // console.log(randomNumber);
   
-  if (randomNumber > 0.5) {
-    throw new Error("Error occurred");
-  }
+  // if (randomNumber > 0.5) {
+  //   throw new Error("Error occurred");
+  // }
 
   return (
     <>
